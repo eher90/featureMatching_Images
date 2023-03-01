0edf83b412827e7ff171ae9c5e8574cb05cb9858
eher78 <abrahamschaecher@outlook.com>
2023-03-01 14:50:07 -0600

    Added SIFT Implementation for Images

--- bruceForceFeatureMatching.py
+++ bruceForceFeatureMatching.py
@@ -1,4 +1,4 @@
-# BRUTE FORCE METHOD
+# BRUTE FORCE METHOD using ORB
 
 # Import necessary libraries
 import numpy as np 
@@ -28,3 +28,22 @@ matches = sorted(matches, key=lambda x:x.distance)
 img3 = cv.drawMatches(image1, kp1, image2, kp2, matches[:20], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 plt.imshow(img3), plt.show()
 print(matches)
+
+
+# USING SIFT Descriptors
+sift = cv.SIFT_create()
+
+kp1, des1 = sift.detectAndCompute(image1,None)
+kp2, des2 = sift.detectAndCompute(image2,None)
+
+bf = cv.BFMatcher()
+matches = bf.knnMatch(des1,des2,k=2)
+
+good = []
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+
+img3 = cv.drawMatchesKnn(image1,kp1,image2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+plt.imshow(img3),plt.show()
