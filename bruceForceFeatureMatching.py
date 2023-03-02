3f7e493fa224c0467dd83cdbf567e07ced25310c
eher78 <abrahamschaecher@outlook.com>
2023-03-02 14:45:00 -0600

    Added SIFT and FLANN analyzer in the loop to compare SIFT.

--- bruceForceFeatureMatching.py
+++ bruceForceFeatureMatching.py
@@ -57,3 +57,29 @@ for x in names:
     img3 = cv.drawMatchesKnn(image1,kp1,image2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
     plt.imshow(img3),plt.show()
+    print(matches)
+
+    # SIFT and FLANN
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks=50)   # or pass empty dictionary
+
+    flann = cv.FlannBasedMatcher(index_params,search_params)
+    matches = flann.knnMatch(des1,des2,k=2)
+
+    # Need to draw only good matches, so create a mask
+    matchesMask = [[0,0] for i in range(len(matches))]
+
+    # ratio test as per Lowe's paper
+    for i,(m,n) in enumerate(matches):
+        if m.distance < 0.7*n.distance:
+            matchesMask[i]=[1,0]
+
+    draw_params = dict(matchColor = (0,255,0),
+                    singlePointColor = (255,0,0),
+                    matchesMask = matchesMask,
+                    flags = cv.DrawMatchesFlags_DEFAULT)
+    
+    img3 = cv.drawMatchesKnn(image1,kp1,image2,kp2,matches,None,**draw_params)
+    plt.imshow(img3,),plt.show()
