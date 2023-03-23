646f3ac5fbc0b04479419c5dc1bf9d82f862fc8b
eher78 <abrahamschaecher@outlook.com>
2023-03-22 21:31:27 -0500

    Cropped Heemskerck Data for better Analysis

--- colorExtraction/percentageOfColor.py
+++ colorExtraction/percentageOfColor.py
@@ -14,7 +14,7 @@
 import cv2
 import matplotlib.pyplot as plt
 
-image = cv2.imread('featureMatching_Images/images/artemisDrawing.jpg')
+image = cv2.imread('featureMatching_Images/artistsImages/heemskerck_01.png')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 histogram = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
